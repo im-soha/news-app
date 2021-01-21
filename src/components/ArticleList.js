c99c55d33a0135f678d5262d033823a8212aca2e
im-soha <75729145+im-soha@users.noreply.github.com>
2021-01-21 21:20:19 +0530

    Update ArticleList.js

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -7,21 +7,21 @@ let toggleState = [false, false, false, false, false, false, false, false, false
 const ArticleItem = (props) => {
   const {article} = props;
   return (
-    <div className="result">
+    <div className="row g-0">
+      <div class="col-md-4">
       {article.media.main_image 
-        ? <img src={article.media.main_image} className="img-card" />
-        : <img src={dummy_image} className="img-card" />}  
-      <h4 className="title">{article.title}</h4>
-      <p className="desc">{article.text}</p>
-      <p className="published">{article.published.substring(0,10)}</p>
-      {/* <div className="card" style="width: 18rem;">
-        <img src="..." className="card-img-top" alt="..."/>
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+        ? <img src={article.media.main_image} id="img-card" className="" alt="news"/>
+        : <img src={dummy_image} id="img-card" className="" alt="news"/>}
+      </div>  
+      <div class="col-md-8">
+        <div class="card-body">
+          <h4 id="title" className="card-header bg-transparent">{article.title}</h4>
+          <p className="summ">{article.summary}</p>
+          {/* <p className="desc">{article.text.substring(0,250)}</p> */}
+            <a href={article.url} target="_blank" className="btn btn-warning">View full article</a>
+            <p className="published">Published on {article.published.substring(8,10)}-{article.published.substring(5,7)}-{article.published.substring(0,4)}</p>
         </div>
-      </div> */}
+      </div> 
     </div>
   );
 };
@@ -30,24 +30,25 @@ const ArticleList = (props) => {
 
     let listing = [];
     if(props.articles) {
-      props.articles.map((news,i) => (
-        listing.push(<li key={i} className="list-group-item" id={"item#"+i} onClick={()=> {
+      props.articles.map((news,i) => {
+          listing.push(<li key={i} className="card mb-3"  id={"item#"+i} onClick={()=> {
           toggleState[i] = !toggleState[i];
           if(toggleState[i]) {
-            document.getElementsByClassName("desc")[i].setAttribute('style', 'display:block');
+            document.getElementsByClassName("summ")[i].setAttribute('style', 'display:block');
           }
           else {
-            document.getElementsByClassName("desc")[i].setAttribute('style', 'display:none');
+            document.getElementsByClassName("summ")[i].setAttribute('style', 'display:none');
           }
         }}>
           <ArticleItem key={news.article.uuid} article={news.article}/>
-        </li>)
-      ));
+        </li>); 
+    });
+
     }
 
   return (
       listing.length>0
-      ? <ul className="list-group list-group-flush">{listing}</ul>
+      ? <ul id="final-list">{listing}</ul>
       : "Loading..."
   );
 };
